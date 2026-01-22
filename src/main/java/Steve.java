0934ec754d10983c84a2842ec151c48097eab82e
dinvsh <127860841+dinvsh@users.noreply.github.com>
2026-01-22 18:48:48 +0800

    Level-0: Rename Duke to Steve, add greeting and exit

---
+++ src/main/java/Steve.java
@@ -0,0 +1,13 @@
+public class steve {
+    public static void main(String[] args) {
+        String name = "steve";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm " + name);
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
